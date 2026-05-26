7b049ea1d3e57ac7260b7d25b50661aeba50743a
Hxtreme <rwpttech@gmail.com>
2026-05-26 10:52:23 +0530

    config

---
+++ include/footer.php
@@ -0,0 +1,189 @@
+<footer class="footer">
+  <div class="footer-quote wow mb-0 fadeIn">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-3"> 
+          <div class="footer-logo-sec">
+          <img class="footer-logo" src="images/infosoft.png" alt="Image">
+          </div>
+          <!-- <div class="social-meadia">
+            <ul class="footer-ul">
+              <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul> 
+          </div> -->
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="footer-heading">
+            <h4>Explore More</h4>
+          </div>
+          <div class="footer-sub-heading">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="about.php">About us</a></li>
+              <!-- <li><a href="services.php">Services</a></li> -->
+              <li><a href="portfolio.php">Portfolio</a></li>
+              <li><a href="clients.php">Clients</a></li>
+              <!-- <li><a href="career.php">Career</a></li> -->
+              <li><a href="contact.php">Contact us</a></li>
+
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="footer-heading">
+            <h4>Services</h4>
+          </div>
+          <div class="footer-sub-heading">
+            <ul>
+              <li><a href="digital.php">Digital
+Marketing</a></li>
+              <li><a href="website.php">Website
+Designing</a></li>
+              <li><a href="e-commerce.php">E-commerce</a></li>
+              <li><a href="mobile-app.php">Mobile
+Applications</a></li>
+
+            </ul>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="footer-heading">
+            <h4>Contact us</h4>
+          </div>
+          <div class="footer-sub-heading">
+            <ul>
+            <li><i class="fa-solid fa-location-dot mr-2"></i>Ludhiana, Punjab</li>
+              <li><i class="fa-solid fa-envelope mr-2"></i><a href="mailto:info@infosoftnetwork.com" class="mail">info@infosoftnetwork.com</a></li>
+              <li><i class="fa-solid fa-phone mr-2"></i><a href="tel:+91 98142 01323" class="call">+91 98142 01323</a></li>
+          
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+ 
+
+</footer>
+
+<!-- <footer class="footer">
+  <div class="footer-quote wow fadeIn">
+    <div class="container">
+      <div class="row">
+      
+        </div>
+        <div class="row">
+        <div class="col-lg-8"> 
+          <h2>Let's create the flexible website for your business ?</h2>
+        </div>
+        </div>
+       
+<div class="row mt-5">
+  <div class="col-12 col-md-3 col-lg-2 footer-heading">
+    <a href="index.php">Home</a>
+  </div>
+  <div class="col-12 col-md-3 col-lg-2 footer-heading">
+    <a href="about.php">About us</a>
+  </div>
+  <div class="col-12 col-md-3 col-lg-2 footer-heading">
+    <a href="services.php">Services</a>
+  </div>
+  <div class="col-12 col-md-3 col-lg-2 footer-heading">
+    <a href="portfolio.php">Portfolio</a>
+  </div>
+  <div class="col-12 col-md-3 col-lg-2 footer-heading">
+    <a href="clients.php">Clients</a>
+  </div>
+  <div class="col-12 col-md-3 col-lg-2 footer-headings">
+    <a href="contact.php">Contact us</a>
+  </div>
+</div>
+ 
+    </div>
+
+  </div>
+
+  <div class="footer-contact wow fadeIn">
+    <div class="container">
+      <div class="row"> 
+        <div class="col-md-4">
+          <div class="d-flex">
+
+      <i class="fa-solid fa-location-dot footericons"></i>
+       <address>
+        Ludhiana, Punjab<br>
+        
+          </address>
+        </div>
+        </div>
+  
+        <div class="col-md-4">
+        <div class="d-flex">
+      <i class="fa-solid fa-phone footericons"></i>
+          <address>
+      
+          <a href="tel:+919814201323" class="">+9198142-01323</a>
+          </address>
+        </div>
+        </div>
+
+        <div class="col-md-3">
+        <div class="d-flex">
+      <i class="fa-regular fa-envelope footericons"></i>
+          <address>
+   
+          <a href="mailto:info@infosoftnetwork.com" class="color-white">info@infosoftnetwork.com</a>
+          </address>
+          </div>
+        </div>
+    
+      </div>
+    </div>
+ 
+  </div>
+
+
+
+</footer> -->
+  <!-- <div class="footer-bottom wow fadeIn">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <h5>Connect with us </h5>
+          <ul>
+            <li><a href="#">Behance</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Vimeo</a></li>
+            <li><a href="#">Facebook</a></li>
+          </ul>
+        </div>
+ 
+      </div>
+  
+    </div>
+  
+  </div> -->
+<!-- end footer -->
+<!-- <audio id="hamburger-hover" src="audio/link.mp3" preload="auto"></audio> -->
+
+<!-- JS FILES --> 
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="js/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/swiper.min.js"></script> 
+<script src="js/tilt.jquery.js"></script> 
+<script src="js/wow.min.js"></script> 
+<script src="js/odometer.min.js"></script> 
+<script src="js/jquery.typewriter.js"></script> 
+<script src="js/fancybox.min.js"></script> 
+<script src="js/app.js"></script>
+<script src="js/scripts.js"></script>
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="theme-color" content="#ff5900"/>
+<meta name="HandheldFriendly" content="true" />
+<title>Infosoft </title>
+
+
+<!-- FAVICON FILES -->
+<link href="ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">
+<link href="ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">
+<link href="ico/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">
+<link href="ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">
+<link href="ico/favicon.png" rel="shortcut icon">
+
+<!-- CSS FILES -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="css/odometer.min.css">
+<link rel="stylesheet" href="css/fancybox.min.css">
+<link rel="stylesheet" href="css/swiper.min.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/services.css">
+</head>
+<body>
+<!-- <div class="preloader-new">
+    <div class="preloader__ring">
+        <div class="preloader__sector">L</div>
+        <div class="preloader__sector">o</div>
+        <div class="preloader__sector">a</div>
+        <div class="preloader__sector">d</div>
+        <div class="preloader__sector">i</div>
+        <div class="preloader__sector">n</div>
+        <div class="preloader__sector">g</div>
+        <div class="preloader__sector">.</div>
+        <div class="preloader__sector">.</div>
+        <div class="preloader__sector">.</div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+    </div>
+    <div class="preloader__ring">
+        <div class="preloader__sector">L</div>
+        <div class="preloader__sector">o</div>
+        <div class="preloader__sector">a</div>
+        <div class="preloader__sector">d</div>
+        <div class="preloader__sector">i</div>
+        <div class="preloader__sector">n</div>
+        <div class="preloader__sector">g</div>
+        <div class="preloader__sector">.</div>
+        <div class="preloader__sector">.</div>
+        <div class="preloader__sector">.</div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+        <div class="preloader__sector"></div>
+    </div>
+</div> -->
+<!-- <div class="preloader">
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="inner" data-tilt data-tilt-perspective="2000">
+    <figure class="fadeInUp animated"> <img src="images/preloader.gif" alt="Image"> </figure>
+    <span class="typewriter" id="typewriter"></span> </div>
+
+</div>
+
+<div class="transition-overlay">
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+  <div class="layer"></div>
+</div> -->
+<!-- end transition-overlay -->
+<div class="navigation-menu">
+  <div class="bg-layers"> <span></span> <span></span> <span></span> <span></span> </div>
+  <!-- end bg-layers -->
+  <div class="inner">
+   <div class="menu">
+    <ul>
+      <li><a href="index.php">Home</a>
+      	<!-- <ul>
+      		<li><a href="index.html">Slider</a></li>
+      		<li><a href="index-video.html">Video</a></li>
+      		<li><a href="index-particles.html">Particles</a></li>
+      		<li><a href="index-perspective.html">Perspective</a></li>
+      	</ul> -->
+      </li>
+      <li><a href="about.php">About us</a></li>
+      <!-- <li><a href="Infosoft.html">web development</a>
+        <ul>
+      		<li><a href="#">Static/Dynamic Web Design</a></li>
+      		<li><a href="#">Makeovers/Re-Design</a></li>
+      		<li><a href="#">Content Managed (CMS)</a></li>
+      		<li><a href="#">E-Commerce</a></li>
+      	</ul></li> -->
+      <li><a>Services</a>
+      <ul>
+      		<li><a href="digital.php">Digital Marketing</a></li>
+      		<li><a href="website.php">Website Development</a></li>
+      		<li><a href="e-commerce.php">E-commerce</a></li>
+      		<li><a href="mobile-app.php">Mobile Application</a></li>
+      	</ul>
+    </li>
+      <li><a href="portfolio.php">Portfolio</a></li>
+      <li><a href="clients.php">Clients</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+    </div>
+    <!-- end menu -->
+    <!-- <blockquote>Let's create useful website for you ?</blockquote> -->
+  </div>
+  <!-- end inner --> 
+</div>
+<!-- end navigation-menu -->
+<div class="container">
+<div class="row">
+<nav class="navbar ">
+  <!-- <div class="left"> <a href="direction.html">DIRECTION</a> </div> -->
+  <!-- end left -->
+
+  <div class="col-8 col-md-8 col-lg-6">
+  <div class="logo"> <a href="index.php"><img src="images/infosoft.png" alt="Image"></a> </div>
+</div>
+<div class="col-4 col-md-4 col-lg-6 d-flex justify-content-end">
+
+  <!-- end logo -->
+  <div class="right">
+    <!-- <ul class="language">
+      <li><a href="#">EN</a></li>
+      <li><a href="#">RU</a></li>
+    </ul> -->
+    <div class="hamburger-menu"><b>MENU</b>
+      <div class="hamburger" id="hamburger-menu"> <span></span> <span></span> <span></span> </div>
+    </div>
+    <!-- end hamburger-menu --> 
+  </div>
+  </div>
+
+  
+
+  <!-- end right --> 
+</nav>
+</div>
+</div>
+<!-- end navbar -->
